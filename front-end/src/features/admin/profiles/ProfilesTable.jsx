7fe1bccc7754578963f69d4ab04161dddf579420
gustavo-berti <gutigimenez15@gmail.com>
2025-09-01 14:59:56 -0300

    feat: adiciona update de perfil

--- front-end/src/features/admin/profiles/ProfilesTable.jsx
+++ front-end/src/features/admin/profiles/ProfilesTable.jsx
@@ -2,10 +2,12 @@ import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
 import ProfileService from '../../../services/profileService';
 import { useEffect, useState } from 'react';
+import { InputText } from 'primereact/inputtext';
 
 const ProfilesTable = () => {
     const profileService = new ProfileService();
     const [profiles, setProfiles] = useState([]);
+
     async function fetchProfiles() {
         try {
             const result = await profileService.getAll();
@@ -14,14 +16,34 @@ const ProfilesTable = () => {
             console.log(error)
         }
     }
+
     useEffect(() => {
         fetchProfiles();
     }, [])
 
+    const onRowEditComplete = (e) => {
+        let _profiles = [...profiles];
+        _profiles[e.index] = e.newData;
+        setProfiles(_profiles);
+        console.log(e.newData);
+        profileService.update(e.newData);
+    }
+
+    const typeEditor = (options) => {
+        return (
+            <InputText
+                value={options.value}
+                style={{textTransform: 'uppercase'}}
+                onChange={(e) => options.editorCallback(e.target.value.toUpperCase())}
+            />
+        )
+    }
+
     return (
-        <DataTable value={profiles}>
-            <Column field="id" header="ID" />
-            <Column field="type" header="Perfil" />
+        <DataTable value={profiles} editMode='row' dataKey="id" onRowEditComplete={onRowEditComplete}>
+            <Column field="id" header="ID"/>
+            <Column field="type" header="Perfil" editor={typeEditor} />
+            <Column rowEditor header="Editar" bodyStyle={{textAlign: 'left'}}/>
         </DataTable>
     )
 }
